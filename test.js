f5251168548e808d4d8405a97409f8fc6a935cb6
mahmudpython <mdmhamud3311@gmail.com>
2026-03-11 22:44:28 +0600

    Re Cheek Link

--- test.js
+++
@@ -1,8 +0,0 @@
-const synonyms=["fdf","fdfd"];
-
-const createElements = (arr) =>{
-    const htmlElemet = arr.map(el=>`<span class="btn">${el}</span>`)
-    return htmlElemet.join(" ");
-}
-
-createElements(synonyms);
